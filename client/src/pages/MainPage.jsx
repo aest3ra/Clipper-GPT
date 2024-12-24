58ddd07cc9eed42a362b26092320187ab58b95ad
aestera <aest3ra@gmail.com>
2024-12-25 02:32:28 +0900

    [frontend] Feat: 메인페이지 완성

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -20,12 +20,15 @@ export default function MainPage() {
                 <p>🚀 현재는 여행 영상에 한해 제공됩니다.</p>
             </div>
         </div>
+    {/* ---------------------------------------------------------------   */}
 
       {/* 버튼 그룹 */}
       <div className="buttons">
         <button className="start-button">편집 시작</button>
         <button className="guide-button">사용 방법</button>
       </div>
+
+    {/* ---------------------------------------------------------------   */}
 
       {/* 사용 방법 */}
       <br></br><br></br><br></br><br></br>
@@ -54,7 +57,11 @@ export default function MainPage() {
                 <div className="desc">
                     <img src="/icon2.png" alt="step2" className="icon" />
                     <h2>Step 2</h2>
-                    <p>원하는 동영상을 업로드해주세요.</p>
+                    <p>
+                        편집하실 영상을 업로드해주세요.
+                        <br />
+                        (장소 효과를 추가하고 싶으시다면 오른쪽 연필 아이콘을 눌러 장소를 추가해주세요.)
+                    </p>
                 </div>
             </div>
 
@@ -65,12 +72,61 @@ export default function MainPage() {
                 <div className="desc">
                     <img src="/icon1.png" alt="step3" className="icon" />
                     <h2>Step 3</h2>
-                    <p>AI가 자동으로 편집을 완료해드립니다!</p>
+                    <p>
+                        이제 모든 과정이 끝났어요.
+                        <br />
+                        AI가 영상을 편집한 뒤 이메일로 보내드릴게요.
+                        <br />
+                        (해당 과정은 영상에 따라 1시간 ~ 3일정도 소요될 수 있습니다.)
+                    </p>
                 </div>
             </div>
         </div>
+    {/* ---------------------------------------------------------------   */}
+        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+        <div className="content">
+            <div className="freeDesc">
+                    프리런칭 2025년 3월까지 무료로 이용해보세요.<br />
+                    프리런칭 기간 중 이용하신 분들께는 정식 런칭 후 무료 쿠폰을 드려요!
+            </div>
+            <div className="logoImg">
+                <img src="/logoBig.svg" alt="logo" />
+            </div>
+        </div>
+        {/* 버튼 그룹 */}
+        <div className="buttons">
+            <button className="start-button">편집 시작</button>
+            <button className="guide-button">사용 방법</button>
+        </div>
 
-      
+    {/* ---------------------------------------------------------------   */}
+        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+
+        <h2>자주 묻는 질문</h2>
+        <div className="ask">
+            <div className="question">
+                무료인가요
+            </div>
+            <div className="question">
+                이메일이 오지 않아요.
+            </div>
+            <div className="question">
+                개인정보 유출 등 우려는 없나요?
+            </div>
+        </div>
+
+    {/* ---------------------------------------------------------------   */}
+        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+
+        <div className="content">
+            <div className="freeDesc">
+                문제가 발생하거나 더 궁금하신 점이 있다면 clippergpt.ai@gmail.com 으로 
+                <br />
+                메일 주시면 빠른 시일 내에 답변 드리도록 하겠습니다.
+            </div>
+        </div>
+
+
     </div>
   );
 }
