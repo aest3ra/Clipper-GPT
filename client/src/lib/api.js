1cbd64aa5c871cf933403a4f0f083a88754e3f09
aestera <aest3ra@gmail.com>
2024-12-28 19:54:59 +0900

    [frontend] feat: 위치 정보 전송 추가

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,22 +1,23 @@
 const baseURL = "http://127.0.0.1:8000";
 
 export async function uploadVideos(subtitle, emailFields, uploadedFiles) {
-
     const formData = new FormData();
 
     formData.append("subtitle", subtitle);
     formData.append("emails", JSON.stringify(emailFields));
-    
-    uploadedFiles.forEach(({ file }) => {
-		formData.append("videos", file);
+
+    uploadedFiles.forEach(({ file, location }) => {
+        formData.append("videos", file);
+        formData.append("locations", location || ""); // 위치 정보 추가 (없으면 빈 값)
     });
 
-    console.log(subtitle)
-	
-	const response = await fetch(baseURL + "/edit/upload", {
-		method: "POST",
-		body: formData,
+    console.log("Subtitle:", subtitle);
+    console.log("Uploaded Files:", uploadedFiles);
+
+    const response = await fetch(baseURL + "/edit/upload", {
+        method: "POST",
+        body: formData,
     });
-  
+
     return response;
 }
--- client/src/pages/edit/GetVideoPage.jsx
+++ client/src/pages/edit/GetVideoPage.jsx
@@ -11,9 +11,9 @@ export default function GetVideoPage() {
     const { handleRoute } = useHandleRoute();
     const location = useLocation();
 
-    const emailFields = location.state?.emailFields || [];
-    const [uploadedFiles, setUploadedFiles] = useState([]);
-    const [isNextDisabled, setIsNextDisabled] = useState(false);
+    const emailFields = location.state?.emailFields || [];          // 입력 이메일
+    const [uploadedFiles, setUploadedFiles] = useState([]);         // 입력 동영상
+    const [isNextDisabled, setIsNextDisabled] = useState(false);    
     const [isLoading, setIsLoading] = useState(false);
     const [subtitleChecked, setSubtitleChecked] = useState(false);
     const [isEditingLocation, setIsEditingLocation] = useState(null);
@@ -31,7 +31,7 @@ export default function GetVideoPage() {
     };
 
     const calculateTotalSize = (files) => {
-      return files.reduce((total, file) => total + file.file.size, 0);
+        return files.reduce((total, file) => total + file.file.size, 0);
     };
 
     const calculateTotalDuration = async (files) => {
@@ -106,157 +106,158 @@ export default function GetVideoPage() {
             alert("업로드할 동영상이 없습니다.");
             return;
         }
-
-    try {
-
-        setIsLoading(true);
-        console.log(uploadedFiles)
-        const response = await uploadVideos(
-            subtitleChecked ? 1 : 0,
-            emailFields,  
-            uploadedFiles  
-        );
-
-        if (!response.ok) {
-            console.error("업로드 실패", response.statusText);
-            alert("업로드 실패");
+    
+        try {
+            setIsLoading(true);
+            console.log("Uploading Files:", uploadedFiles);
+    
+            const response = await uploadVideos(
+                subtitleChecked ? 1 : 0,
+                emailFields,
+                uploadedFiles
+            );
+    
+            if (!response.ok) {
+                console.error("업로드 실패", response.statusText);
+                alert("업로드 실패");
+                setIsLoading(false);
+                return;
+            }
+    
+            handleRoute("/complete");
+        } catch (error) {
+            console.error("업로드 에러", error);
+            alert("업로드 중 오류가 발생했습니다.");
             setIsLoading(false);
-            return;
         }
+    };
+    
 
-        handleRoute("/complete");
-    } catch (error) {
-        console.error("업로드 에러", error);
-        alert("업로드 중 오류가 발생했습니다.");
-        setIsLoading(false);
-    }
-  };
-
-  return (
-    <div className={styles.root}>
-    {isLoading && (
-        <div className={styles.loadingOverlay}>
-          <Spinner />
-        </div>
-    )}
-
-    <h2 className={styles.title}>Upload video</h2>
-
-    {uploadedFiles.length === 0 ? (
-        <div
-          className={styles.videoInput}
-          onDragOver={(e) => e.preventDefault()}
-          onDrop={handleDrop}
-        >
-          <label htmlFor="fileInput" className={styles.dropContent}>
-            <img src="/drop.svg" alt="Drop Icon" className={styles.dropIcon} />
-            <p>
-              <span className={styles.uploadLink}>Click to Upload</span> or drag and drop
-            </p>
-            <p className={styles.fileSize}>Max. number of video 5</p>
-          </label>
-          <input
-            id="fileInput"
-            type="file"
-            accept="video/*"
-            multiple
-            className={styles.fileInput}
-            onChange={handleFileSelect}
-          />
-        </div>
-      ) : (
-        <>
-            <div className={styles.fileList}>
-            {uploadedFiles.map((item, index) => (
-                <div key={index} className={styles.fileItem}>
-                    <div className={styles.fileDetails}>
-                        <img src="/video.svg" alt="Video Icon" className={styles.fileIcon} />
-                        <div>
-                            <p className={styles.fileName}>
-                                {item.name.length > 20 ? item.name.slice(0, 20) + "..." : item.name}
-                            </p>
-                            <p className={styles.fileSize}>{item.displaySize}</p>
-                            {isEditingLocation === index ? (
-                                <input
-                                    type="text"
-                                    placeholder="Enter location"
-                                    className={styles.locationInput}
-                                    onBlur={(e) => handleLocationChange(index, e.target.value)}
-                                    autoFocus
-                                />
-                                ) : (
-                                <p
-                                    className={styles.addLocation}
-                                    onClick={() => setIsEditingLocation(index)}
-                                >
-                                    {item.location || "Click to add location"}
-                                </p>
-                            )}
-                        </div>
-                    </div>
-                <img
-                  src="/trash.svg"
-                  alt="Trash Icon"
-                  className={styles.trashIcon}
-                  onClick={() => handleRemoveFile(index)}
-                />
-              </div> 
-            ))}
+    return (
+        <div className={styles.root}>
+        {isLoading && (
+            <div className={styles.loadingOverlay}>
+            <Spinner />
             </div>
+        )}
 
+        <h2 className={styles.title}>Upload video</h2>
 
-
-          <div className={styles.addButton}>
-            <button
-              className={styles.addVideoButton}
-              onClick={() => document.getElementById("fileInput").click()}
+        {uploadedFiles.length === 0 ? (
+            <div
+                className={styles.videoInput}
+                onDragOver={(e) => e.preventDefault()}
+                onDrop={handleDrop}
             >
-              + Add Video
-            </button>
-            
-            <input
-              id="fileInput"
-              type="file"
-              accept="video/*"
-              multiple
-              className={styles.fileInput}
-              onChange={handleFileSelect}
-            />
-          </div>
-        </>
-      )}
-
-      {uploadedFiles.length > 0 && (
-        <>
-          <div className={styles.checkboxContainer}>
-            <div className={styles.checkboxContainer}>
-              <label className={styles.checkboxLabel}>
+                <label htmlFor="fileInput" className={styles.dropContent}>
+                    <img src="/drop.svg" alt="Drop Icon" className={styles.dropIcon} />
+                    <p>
+                        <span className={styles.uploadLink}>Click to Upload</span> or drag and drop
+                    </p>
+                    <p className={styles.fileSize}>Max. number of video 5</p>
+                </label>
                 <input
-                  type="checkbox"
-                  checked={subtitleChecked}
-                  onChange={(e) => setSubtitleChecked(e.target.checked)}
-                  className={styles.checkbox}
+                    id="fileInput"
+                    type="file"
+                    accept="video/*"
+                    multiple
+                    className={styles.fileInput}
+                    onChange={handleFileSelect}
                 />
-                자막 씌우기
-              </label>
             </div>
-          </div>
+        ) : (
+            <>
+                <div className={styles.fileList}>
+                {uploadedFiles.map((item, index) => (
+                    <div key={index} className={styles.fileItem}>
+                        <div className={styles.fileDetails}>
+                            <img src="/video.svg" alt="Video Icon" className={styles.fileIcon} />
+                            <div>
+                                <p className={styles.fileName}>
+                                    {item.name.length > 20 ? item.name.slice(0, 20) + "..." : item.name}
+                                </p>
+                                <p className={styles.fileSize}>{item.displaySize}</p>
+                                {isEditingLocation === index ? (
+                                    <input
+                                        type="text"
+                                        placeholder="Enter location"
+                                        className={styles.locationInput}
+                                        onBlur={(e) => handleLocationChange(index, e.target.value)}
+                                        autoFocus
+                                    />
+                                    ) : (
+                                    <p
+                                        className={styles.addLocation}
+                                        onClick={() => setIsEditingLocation(index)}
+                                    >
+                                        {item.location || "Click to add location"}
+                                    </p>
+                                )}
+                            </div>
+                        </div>
+                    <img
+                        src="/trash.svg"
+                        alt="Trash Icon"
+                        className={styles.trashIcon}
+                        onClick={() => handleRemoveFile(index)}
+                    />
+                </div> 
+                ))}
+                </div>
 
-          <div className={styles.buttons}>
-            <button
-              className={styles.startButton}
-              disabled={isNextDisabled}
-              onClick={handleUploadAndNext}
-            >
-              다음
-            </button>
-          </div>
-        </>
-      )}
 
-      <div className={styles.graph}>
-        <Step2 />
-      </div>
-    </div>
-  );
+
+                <div className={styles.addButton}>
+                    <button
+                        className={styles.addVideoButton}
+                        onClick={() => document.getElementById("fileInput").click()}
+                    >
+                    + Add Video
+                    </button>
+                    
+                    <input
+                        id="fileInput"
+                        type="file"
+                        accept="video/*"
+                        multiple
+                        className={styles.fileInput}
+                        onChange={handleFileSelect}
+                    />
+                </div>
+            </>
+        )}
+
+        {uploadedFiles.length > 0 && (
+            <>
+            <div className={styles.checkboxContainer}>
+                <div className={styles.checkboxContainer}>
+                <label className={styles.checkboxLabel}>
+                    <input
+                    type="checkbox"
+                    checked={subtitleChecked}
+                    onChange={(e) => setSubtitleChecked(e.target.checked)}
+                    className={styles.checkbox}
+                    />
+                    자막 씌우기
+                </label>
+                </div>
+            </div>
+
+            <div className={styles.buttons}>
+                <button
+                className={styles.startButton}
+                disabled={isNextDisabled}
+                onClick={handleUploadAndNext}
+                >
+                다음
+                </button>
+            </div>
+            </>
+        )}
+
+        <div className={styles.graph}>
+            <Step2 />
+        </div>
+        </div>
+    );
 }
